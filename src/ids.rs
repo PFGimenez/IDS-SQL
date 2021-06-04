24f1432307722b09b60e4cdb1ba847f68e8fdc65
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2021-06-04 23:04:43 +0200

    Simplifying code

--- src/ids.rs
+++ src/ids.rs
@@ -1,12 +1,16 @@
 use sqlparser::tokenizer::Tokenizer;
 use sqlparser::dialect::GenericDialect;
 use sqlparser::tokenizer::Token;
+use sqlparser::tokenizer::Word;
 use sqlparser::tokenizer::Whitespace;
+
+//use std::sync::RwLock TODO
 
 use std::collections::HashMap;
 
 use crate::template::Template;
 
+const MINIMUM_QUERIES_FOR_LEARNING: u32 = 5;
 
 #[derive(Debug)]
 pub enum ReqFate {
@@ -16,7 +20,7 @@ pub enum ReqFate {
 
 pub struct Ids {
     templates: Vec<Template>,
-    lonely_queries: HashMap<usize, Vec<String>>
+    lonely_queries: HashMap<Vec<Token>, Vec<String>>
 }
 
 impl Ids {
@@ -25,7 +29,8 @@ impl Ids {
     }
 
     pub fn handle_req(&mut self, req: &str) -> ReqFate {
-        let tokens = Ids::tokenize_without_whitespace(req);
+        let tokens = Ids::tokenize(req);
+        println!("{:?}",tokens);
         let mut match_found = false;
         for t in self.templates.iter() {
             if t.is_match(req)
@@ -41,37 +46,44 @@ impl Ids {
         if match_found {
             ReqFate::Del
         } else {
-            self.learn(req, tokens);
+            self.learn(req, tokens); // TODO dans un thread ?
             ReqFate::Pass(None)
         }
     }
 
     fn learn(&mut self, req: &str, tokens: Vec<Token>) {
-        let n = tokens.len();
-        let queries = self.lonely_queries.entry(n).or_insert(Vec::new());
+        let queries = self.lonely_queries.entry(tokens).or_insert(Vec::new());
         queries.push(req.to_string());
-        let u = 3;
-        let a = "SELECT * FROM";
-        let b = "WHERE A=1";
-        let t = Template::new(&vec![a,b], vec![(tokens[u].clone(),u)]);
-        self.templates.push(t);
+        if queries.len() >= MINIMUM_QUERIES_FOR_LEARNING
+        {
+
+        }
+        // let t = Template::new(&vec![a,b], vec![(tokens[u].clone(),u)]);
+        // self.templates.push(t);
         // println!("Legitimate: {}", t.is_legitimate(&tokens));
 
     }
 
-    fn tokenize_without_whitespace(req: &str) -> Vec<Token> {
-        let c = |t : &Token| {
-            // the user may add whitespace but not comment !
+    fn tokenize(req: &str) -> Vec<Token> {
+        // the user may add whitespace but not comment !
+        // remove the value of the tokens as well
+        let filter_map = |t : Token| {
             match t {
-                Token::Whitespace(Whitespace::Space) => false,
-                Token::Whitespace(Whitespace::Newline) => false,
-                Token::Whitespace(Whitespace::Tab) => false,
-                _ => true
+                Token::Word(w) => Some(Token::Word(Word { value: String::new(), quote_style: None, keyword: w.keyword })), // keep only the keyword
+                Token::Number(_,_) => Some(Token::Number(String::from("0"),true)),
+                Token::Char(_) => Some(Token::Char(' ')),
+                Token::SingleQuotedString(_) => Some(Token::SingleQuotedString(String::new())),
+                Token::NationalStringLiteral(_) => Some(Token::NationalStringLiteral(String::new())),
+                Token::HexStringLiteral(_) => Some(Token::HexStringLiteral(String::new())),
+                Token::Whitespace(Whitespace::Space) => None,
+                Token::Whitespace(Whitespace::Newline) => None,
+                Token::Whitespace(Whitespace::Tab) => None,
+                _ => Some(t)
             }
         };
 
         let dialect = GenericDialect {};
-        Tokenizer::new(&dialect, req).tokenize().unwrap().into_iter().filter(c).collect()
+        Tokenizer::new(&dialect, req).tokenize().unwrap().into_iter().filter_map(filter_map).collect()
     }
 
 
--- src/template.rs
+++ src/template.rs
@@ -11,7 +11,7 @@ impl Template {
     pub fn new(template_parts: &Vec<&str>, inj_tok: Vec<(Token,usize)>) -> Template {
         assert_eq!(inj_tok.len() + 1, template_parts.len());
         let mut iter = template_parts.iter();
-        let mut regex = "^".to_string() + &regex::escape(iter.next().unwrap());
+        let mut regex = String::from("^") + &regex::escape(iter.next().unwrap());
         loop
         {
             // construct the RE in the form : ^ p1 .* p2 .* p3 $
@@ -35,21 +35,9 @@ impl Template {
         for t in 1..self.inj_tok.len() { // only check the injections
             let (t1,u) = &self.inj_tok[t];
             let t2 = &input[*u];
-            match (t1,t2) {
-                    (Token::Word(w1),Token::Word(w2)) =>
-                        if w1.keyword != w2.keyword {
-                            return false // only compare the type of the keyword and not the value
-                        },
-                    (Token::Number(_,_),Token::Number(_,_)) => (), // both numbers, don't check the value
-                    (Token::Char(_),Token::Char(_)) => (), // both chars, don't check the value
-                    (Token::SingleQuotedString(_),Token::SingleQuotedString(_)) => (), // both strings, don't check the value
-                    (Token::NationalStringLiteral(_),Token::NationalStringLiteral(_)) => (),
-                    (Token::HexStringLiteral(_),Token::HexStringLiteral(_)) => (),
-                    (o1,o2) =>
-                        if o1 != o2 {
-                            return false // different type
-                        }
-                }
+            if t1 != t2 {
+                return false
+            }
         }
         true
     }
