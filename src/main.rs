c9224eac10fdd591024d34e4f84ad9e43348cf50
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2021-06-09 10:00:16 +0200

    Read from file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+use std::io::BufReader;
+use std::fs::File;
+use std::io::BufRead;
+
 mod template;
 mod ids;
 mod tokens;
 use ids::Ids;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut ids = Ids::new();
-    println!("{}", ids.handle_req("SELECT * FROM table ; SELECT * FROM users WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table1 WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table2 WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table5 WHERE B=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table5 WHERE B=\'2\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table3 WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table4 WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table5 WHERE A=\'1\'"));
-    println!("{}", ids.handle_req("SELECT * FROM table ; SELECT * FROM users WHERE A=\'1\'"));
+
+    let f = File::open("queries.txt")?;
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let l = line.unwrap();
+        // println!("{}", l); 
+        let fate = ids.handle_req(&l);
+        println!("  {}", fate);
+    }
+
+    Ok(())
 }
 
 
